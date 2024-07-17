43583f91fa98cbedf1c6f4dc9743ac226b45c65c
Robin <vougishtiger187@gmail.com>
2024-07-16 22:24:47 -0700

    main

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import {puppyList} from './data.js'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log(puppyList)
+  return (
+    <>
+      <div>{
+        puppies.map((puppy)=> {
+          return <p>{puppy.name}</p>
+        })}
+        
+      </div>
+      
+  
+    </>
+  )
+}
+
+export default App
